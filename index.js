4fe2fc37bab85769a5219835e20395343a172dc1
RyxDisini <ryxvip1@gmail.com>
2024-06-03 09:49:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { ragBot,
-  degreGuru,
+  degreeGuru,
   pinecoe,
   smartContract,
   blackboxAIChat,
