2a510d5f98490da67857bb582d6bbab4d81a9e5d
RyxDisini <ryxvip1@gmail.com>
2024-06-03 10:19:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const openaiApiKey = 'sk-proj-9VqHONylbMwzqKyHyseoT3BlbkFJjnd7tZyku2zMhdPfdMww';
+const apikey = 'sk-proj-9VqHONylbMwzqKyHyseoT3BlbkFJjnd7tZyku2zMhdPfdMww';
 const creator = '@ryxx-Apis'
